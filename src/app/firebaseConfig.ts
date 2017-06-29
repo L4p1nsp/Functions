f5d78153bb000c245ded2ec707849c8a3209997e
bayendemba <bayendemba@gmail.com>
2017-06-29 17:39:38 +0000

    Delete Config File
    Delete Config File

--- src/app/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    apiKey: "AIzaSyAMzDRwnmXfZCJXdgLSJPvgn1BRbkAfYVA",
-    authDomain: "fir-functions-7052b.firebaseapp.com",
-    databaseURL: "https://fir-functions-7052b.firebaseio.com",
-    projectId: "fir-functions-7052b",
-    storageBucket: "fir-functions-7052b.appspot.com",
-    messagingSenderId: "725927286274"
-};
